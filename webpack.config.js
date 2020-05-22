ed115dbfd5228b13408a4a63e4234d0e789554c9
Vladyslavvs <57898919+Vladyslavvs@users.noreply.github.com>
2020-05-22 14:15:35 +0300

    finish base framework

--- webpack.config.js
+++ webpack.config.js
@@ -9,61 +9,46 @@ const isDev=!isProd
 
 const filename=(ext)=>isDev?`bundle.${ext}`:`bundle.[hash].${ext}`
 
-const jsLoaders= () =>{
-    const loaders=[
-        {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    ]
-
-    if (isDev)
-        loaders.push('eslint-loader')
-
-    return loaders
-}
 
 module.exports= {
-    context: path.resolve(__dirname, 'src'),
+    context: path.resolve (__dirname, 'src'),
     mode: 'development',
     entry: ['@babel/polyfill', './index.js'],
     output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, 'dist')
+        filename: filename ('js'),
+        path: path.resolve (__dirname, 'dist')
     },
     resolve: {
         extensions: ['.js'],
         alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core')
+            '@': path.resolve (__dirname, 'src'),
+            '@core': path.resolve (__dirname, 'src/core')
         }
     },
-    devtool: isDev?'source-map':false,
+    devtool: isDev ? 'source-map' : false,
     devServer: {
         port: 3000,
         hot: isDev
     },
     plugins: [
-        new CleanWebpackPlugin(),
-        new HTMLWebpackPlugin({
+        new CleanWebpackPlugin (),
+        new HTMLWebpackPlugin ({
             template: 'index.html',
             minify: {
                 removeComments: isProd,
                 collapseWhiteSpace: isProd
             }
         }),
-        new CopyPlugin({
+        new CopyPlugin ({
             patterns: [
                 {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
+                    from: path.resolve (__dirname, 'src/favicon.ico'),
+                    to: path.resolve (__dirname, 'dist')
                 }
             ]
         }),
-        new MiniCssExtraPlugin({
-            filename: filename('css')
+        new MiniCssExtraPlugin ({
+            filename: filename ('css')
         })
     ],
     module: {
@@ -84,8 +69,16 @@ module.exports= {
             },
             {
                 test: /\.js$/,
-                exclude: [path.resolve(__dirname, 'node_modules')],
-                use: jsLoaders()
+                exclude: [path.resolve (__dirname, 'node_modules')],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins:['@babel/plugin-proposal-class-properties']
+                        }
+                    }
+                ]
             }
         ]
     }
